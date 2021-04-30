817dc5ca489b2de82b7015943ba1dfe238324b06
Adrian Yin <adrianyin@yeah.net>
2021-04-30 21:59:18 +0800

    完成webrtc视频通信客户端代码

--- Car/app/src/main/java/com/adrianyin/car/MainActivity.java
+++ Car/app/src/main/java/com/adrianyin/car/MainActivity.java
@@ -5,28 +5,47 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.os.Bundle;
+import android.util.Log;
 
+import org.json.JSONObject;
 import org.webrtc.Camera1Enumerator;
+import org.webrtc.DataChannel;
 import org.webrtc.DefaultVideoDecoderFactory;
 import org.webrtc.DefaultVideoEncoderFactory;
 import org.webrtc.EglBase;
+import org.webrtc.IceCandidate;
+import org.webrtc.MediaConstraints;
+import org.webrtc.MediaStream;
 import org.webrtc.PeerConnection;
 import org.webrtc.PeerConnectionFactory;
+import org.webrtc.RtpReceiver;
+import org.webrtc.SdpObserver;
+import org.webrtc.SessionDescription;
 import org.webrtc.SurfaceTextureHelper;
 import org.webrtc.SurfaceViewRenderer;
 import org.webrtc.VideoCapturer;
 import org.webrtc.VideoSource;
 import org.webrtc.VideoTrack;
 
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
 import pub.devrel.easypermissions.EasyPermissions;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SocketClient.Callback {
+
+    private static final String TAG = "MainActivity";
 
     SurfaceViewRenderer localView;
+    SurfaceViewRenderer remoteView;
 
     PeerConnectionFactory peerConnectionFactory;
+    PeerConnection peerConnection;
     EglBase.Context eglBaseContext;
     VideoTrack videoTrack;
+    MediaStream mediaStream;
+    MediaConstraints mediaConstraints;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,11 +61,21 @@ public class MainActivity extends AppCompatActivity {
                     this, "需要获取相机权限", 0, permissions);
         }
 
+        // 初始化组件
         initComponents();
 
-        // WebRTC传输视频
+        // 创建peerConnectionFactory
         createPeerConnectionFactory();
+        // 配置mediaConstraints
+        setMediaConstraints();
+        // 获取本地视频并创建媒体流
         getLocalVideo();
+        // 连接信令服务器并绑定回调函数
+        SocketClient.get().setCallback(this);
+        // 创建peerConnection
+        createPeerConnection();
+        // 添加本地媒体流到节点
+        peerConnection.addStream(mediaStream);
     }
 
     @Override
@@ -67,6 +96,12 @@ public class MainActivity extends AppCompatActivity {
         localView.setKeepScreenOn(true);
         localView.setEnableHardwareScaler(false);
         localView.init(eglBaseContext, null);
+
+        remoteView = findViewById(R.id.remoteView);
+        remoteView.setMirror(true);
+        remoteView.setKeepScreenOn(true);
+        remoteView.setEnableHardwareScaler(false);
+        remoteView.init(eglBaseContext, null);
     }
 
     // 创建peerConnectionFactory
@@ -126,7 +161,108 @@ public class MainActivity extends AppCompatActivity {
 
         videoTrack = peerConnectionFactory.createVideoTrack("carCamera", videoSource);
         videoTrack.addSink(localView);
+
+        mediaStream = peerConnectionFactory.createLocalMediaStream("mediaSteam");
+        mediaStream.addTrack(videoTrack);
+    }
+
+    private void createPeerConnection() {
+        List<PeerConnection.IceServer> iceServers = new ArrayList<>();
+        iceServers.add(
+                PeerConnection.IceServer
+                        .builder("stun:stun.l.google.com:19302")
+                        .createIceServer()
+        );
+        peerConnection = peerConnectionFactory.createPeerConnection(
+                iceServers,
+                new PeerConnectionAdapter() {
+                    @Override
+                    public void onIceCandidate(IceCandidate iceCandidate) {
+                        super.onIceCandidate(iceCandidate);
+                        SocketClient.get().sendIceCandidate(iceCandidate);
+                    }
+
+                    @Override
+                    public void onAddStream(MediaStream mediaStream) {
+                        super.onAddStream(mediaStream);
+                        VideoTrack remoteVideoTrack = mediaStream.videoTracks.get(0);
+                        runOnUiThread(() -> remoteVideoTrack.addSink(remoteView));
+                    }
+                }
+        );
+    }
+
+    private void setMediaConstraints() {
+        mediaConstraints = new MediaConstraints();
+        mediaConstraints.mandatory.add(new MediaConstraints.KeyValuePair("OfferToReceiveAudio", "false"));
+        mediaConstraints.mandatory.add(new MediaConstraints.KeyValuePair("OfferToReceiveVideo", "true"));
+        mediaConstraints.optional.add(new MediaConstraints.KeyValuePair("DtlsSrtpKeyAgreement", "true"));
     }
 
 
+    // 实现SocketClient事件回调函数
+
+    // 收到offer设置remote sdp；并创建answer同时设置local sdp
+    @Override
+    public void onOfferReceived(JSONObject data) {
+        runOnUiThread(() -> {
+            peerConnection.setRemoteDescription(new SdpAdapter("SetRemote"),
+                    new SessionDescription(SessionDescription.Type.OFFER, data.optString("sdp")));
+            peerConnection.createAnswer(new SdpAdapter("Answer") {
+                @Override
+                public void onCreateSuccess(SessionDescription sessionDescription) {
+                    super.onCreateSuccess(sessionDescription);
+                    peerConnection.setLocalDescription(
+                            new SdpAdapter("SetLocal"), sessionDescription);
+                    SocketClient.get().sendSessionDescription(sessionDescription);
+                }
+            }, mediaConstraints);
+        });
+    }
+
+    // 收到Answer设置remote sdp
+    @Override
+    public void onAnswerReceived(JSONObject data) {
+        peerConnection.setRemoteDescription(new SdpAdapter("SetRemote"),
+                new SessionDescription(SessionDescription.Type.ANSWER, data.optString("sdp")));
+    }
+
+    // 收到候选人信息加入候选人
+    @Override
+    public void onIceCandidateReceived(JSONObject data) {
+        peerConnection.addIceCandidate(new IceCandidate(
+                data.optString("id"),
+                data.optInt("label"),
+                data.optString("candidate")
+        ));
+    }
+
+    @Override
+    public void onCreateRoom() {
+
+    }
+
+    // 后加入的节点发送offer
+    @Override
+    public void onSelfJoined() {
+        peerConnection.createOffer(new SdpAdapter("Offer") {
+            @Override
+            public void onCreateSuccess(SessionDescription sessionDescription) {
+                super.onCreateSuccess(sessionDescription);
+                peerConnection.setLocalDescription(
+                        new SdpAdapter("SetLocal"), sessionDescription);
+                SocketClient.get().sendSessionDescription(sessionDescription);
+            }
+        }, mediaConstraints);
+    }
+
+    @Override
+    public void onPeerJoined() {
+
+    }
+
+    @Override
+    public void onPeerLeave(String message) {
+
+    }
 }
