9a668f5dc2fe82f1518e7e301fdaf22d1f3a9671
Adrian Yin <adrianyin@yeah.net>
2021-04-30 22:44:14 +0800

    完成信令服务器

--- SignalServer/index.js
+++ SignalServer/index.js
@@ -18,6 +18,73 @@ const app = https.createServer(httpsOptions, function(req, res) {
 const io = socketIO.listen(app);
 io.sockets.on('connection', function(socket) {
 
-    // 在此添加信令服务器逻辑
+    let that = this;
 
+    //记录加入房间号
+    let roomName;
+
+    // 向客户端发送服务器日志信息
+    function log() {
+        const array = ['来自服务器的信息：'];
+        array.push.apply(array, arguments);
+        socket.emit('log', array);
+        console.log('向客户端发送日志信息：', array)
+    }
+
+    // 收到消息转发
+    socket.on('message', function(message) {
+        log('收到客户端信息：', message);
+        // 房间内转发消息
+        if (!roomName) {
+            console.log('消息转发失败，连接未加入房间');
+        } else {
+            console.log('在房间' + roomName + '内转发消息：' + message);
+            socket.to(roomName).emit('message', message);
+        }
+    });
+
+    socket.on('create or join', function(roomName) {
+        log('收到创建或加入房间的请求，房间名：' + roomName);
+
+        let clientsInRoom = io.sockets.adapter.rooms[roomName];
+        let numClients = clientsInRoom ? Object.keys(clientsInRoom.socket).length : 0;
+        log(roomName + '房间目前有' + numClients + '个客户端');
+
+        if (numClients === 0) {
+            // 房间不存在，创建房间
+            socket.join(roomName);
+            that.roomName = roomName;
+            log('用户' + socket.id + '创建了房间' + roomName);
+            socket.emit('created', roomName, socket.id);
+        } else if (numClients === 1) {
+            // 房间已有1个客户端，加入房间
+            log('用户' + socket.id + '加入了房间' + roomName);
+            io.sockets.in(roomName).emit('join', roomName);
+            socket.join(roomName);
+            that.roomName = roomName;
+            socket.emit('joined', roomName, socket.id);
+            io.sockets.in(roomName).emit('ready');
+        } else {
+            // 房间已满
+            socket.emit('full', roomName);
+        }
+    });
+
+    socket.on('ipaddr', function() {
+        const ifaces = os.networkInterfaces();
+        for (let dev in ifaces) {
+            if (!ifaces.hasOwnProperty(dev)) {
+                break;
+            }
+            ifaces[dev].forEach(function(details) {
+                if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
+                    socket.emit('ipaddr', details.address);
+                }
+            });
+        }
+    });
+
+    socket.on('bye', function () {
+        console.log('收到关闭连接信息');
+    });
 })
