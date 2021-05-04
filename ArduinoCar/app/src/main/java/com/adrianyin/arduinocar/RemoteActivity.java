2956e4bb5793dc2c55756f3037c44fe3cbdbfaf1
Adrian Yin <adrianyin@yeah.net>
2021-05-04 15:28:00 +0800

    完成自身定位功能

--- ArduinoCar/app/src/main/java/com/adrianyin/arduinocar/RemoteActivity.java
+++ ArduinoCar/app/src/main/java/com/adrianyin/arduinocar/RemoteActivity.java
@@ -1,15 +1,33 @@
 package com.adrianyin.arduinocar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.CameraUpdate;
+import com.amap.api.maps.CameraUpdateFactory;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.UiSettings;
+import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.Marker;
+import com.amap.api.maps.model.MarkerOptions;
+import com.amap.api.maps.model.MyLocationStyle;
+
 public class RemoteActivity extends AppCompatActivity {
 
-    Button toLocalButton;
+    private Button toLocalButton;
+    private MapView mapView;
+
+    private AMap aMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +44,57 @@ public class RemoteActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // 创建地图
+        mapView.onCreate(savedInstanceState);
+        if (aMap == null) {
+            aMap = mapView.getMap();
+        }
+
+        // 配置地图显示内容
+        UiSettings uiSettings = aMap.getUiSettings();
+        uiSettings.setZoomControlsEnabled(false);  // 缩放按钮
+        uiSettings.setAllGesturesEnabled(false); // 禁用所有手势
+
+        // 配置地图缩放级别
+        float zoomLevel = 18.5f;  // 缩放级别3-19，越大越精细
+        aMap.moveCamera(CameraUpdateFactory.zoomTo(zoomLevel));
+
+        // 获取自身定位点
+        MyLocationStyle myLocationStyle = new MyLocationStyle();
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);
+        myLocationStyle.interval(2000);
+        myLocationStyle.showMyLocation(true);
+        aMap.setMyLocationStyle(myLocationStyle);
+        aMap.setMyLocationEnabled(true);
     }
 
     private void init() {
         toLocalButton = findViewById(R.id.toLocalButton);
+        mapView = findViewById(R.id.map);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
     }
 }
